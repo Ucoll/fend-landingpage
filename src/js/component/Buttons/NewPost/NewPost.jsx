085e64599bb75122cf36a59fb78a5cb9f6c80c0b
Ovidio Santoro <jogsantoro@gmail.com>
2022-02-01 23:02:29 +0100

    Created NewPost button and some global styles

---
+++ src/js/component/Buttons/NewPost/NewPost.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+import "./NewPost.css";
+
+/**
+ * ! Creates the NewPost Button
+ * * 2022-02-01
+ * @returns React Component
+ */
+export const NewPost = () => {
+  return (
+    <div className="button-feed">
+      <Icon className="button-icon" icon="akar-icons:plus" />
+    </div>
+  );
+};
