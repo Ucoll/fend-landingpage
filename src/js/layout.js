539c717b88da7c89098d1b7c2fd162eb4a6f5d54
Ovidio Santoro <jogsantoro@gmail.com>
2022-02-02 19:14:40 +0100

    Created the New Coll Link button

--- src/js/layout.js
+++ src/js/layout.js
@@ -10,7 +10,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-import { NewPostLink } from "./component/Buttons/NewPostButtons/NewPostLink.jsx";
+import { NewPostSurvey } from "./component/Buttons/NewPostButtons/NewPostSurvey.jsx";
 
 //create your first component
 const Layout = () => {
@@ -23,7 +23,7 @@ const Layout = () => {
       <BrowserRouter basename={basename}>
         <ScrollToTop>
           <Navbar />
-          <NewPostLink />
+          <NewPostSurvey />
           <Switch>
             <Route exact path="/">
               <Home />
