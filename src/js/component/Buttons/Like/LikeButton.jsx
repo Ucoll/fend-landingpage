5896e9b499c218722d299fac638d20855f31d3da
AslanSN <aslan.staub@manumartinez.es>
2022-02-02 20:55:16 +0100

    added resources

---
+++ src/js/component/Buttons/Like/LikeButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LikeButton = () => {
+    return(
+        <div>
+            <img src="../../../../img/icons/like/LikeAwaken.png" alt="Like Awaken Button" height="600"/>
+            <img src="../../../../img/icons/like/LikeSlept.png" alt="Like Slept Button" height="600"/>
+        </div>
+    );
+}
+
+export default LikeButton;
