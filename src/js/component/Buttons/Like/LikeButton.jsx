50b284c66aa814ffb1a46845ccafc3a37c399b13
AslanSN <aslan.staub@manumartinez.es>
2022-02-04 15:17:26 +0100

    Like button demo done

--- src/js/component/Buttons/Like/LikeButton.jsx
+++ src/js/component/Buttons/Like/LikeButton.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import likeSleptPNG from "../../../../img/icons/like/LikeSlept.png";
-import "./LikeButtonStyle.scss"
+import likeAwakenPNG from "../../../../img/icons/like/LikeAwaken.png";
+import wow1 from "../../../../img/icons/like/WOW 1.png";
+import "./LikeButtonStyle.scss";
 
 /**
  * ! BUTTON => Like
- * * AslanSN 
+ * * AslanSN
  * * 2022-02-02
  * @returns React Component
  */
 const LikeButton = () => {
-    return(
-        <div>
-            <img className="button-like" src={likeSleptPNG} alt="Like Slept Button" height="52" width="52"/>
-        </div>
-    );
-}
+  return (
+    <>
+      <div className="button-like">
+        <img
+         className="like-slept" 
+          src={likeSleptPNG}
+          alt="Like Slept Button"
+          height="52"
+          width="52"
+        />
+        <img
+          className="like-awaken"
+          src={likeAwakenPNG}
+          alt="Like Awaken"
+          height="52"
+          width="52"
+        />
+        <img src={wow1} alt="YAY!" className="wow1" height="52"
+          width="52" />
+
+      </div>
+    </>
+  );
+};
 
 export default LikeButton;
