c6d4c50eae6d8fc07293498c714416c0a91a0bfe
Ovidio Santoro <jogsantoro@gmail.com>
2022-02-06 19:44:16 +0100

    Initial commit

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,13 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
-
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 
 import NewPostVideo from "./component/Buttons/NewPostButtons/NewPostVideo.jsx";
 import NewPostLink from "./component/Buttons/NewPostButtons/NewPostLink.jsx";
