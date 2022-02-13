704249d85fff6df3fe9b588fef019e070bf46ef4
Ovidio Santoro <jogsantoro@gmail.com>
2022-02-13 17:03:27 +0100

    ÂRefactored the Like button: added svg, useState, useEffect changed styles

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,11 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import injectContext from "./store/appContext";
 
-
-
 import LikeButton from "./component/Buttons/Like/LikeButton.jsx";
-
-
 
 import NewPostVideo from "./component/Buttons/NewPostButtons/NewPostVideo.jsx";
 import NewPostLink from "./component/Buttons/NewPostButtons/NewPostLink.jsx";
@@ -30,13 +26,11 @@ import FavoriteOut from "./component/Buttons/Favorite/FavoriteOut/FavoriteOut.js
 import UCollLogo from "./component/Buttons/UCollLogo/UCollLogo.jsx";
 import FavoriteGolden from "./component/Buttons/Favorite/FavoriteGolden/FavoriteGolden.jsx";
 
-
-
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
   return (
     <div>
@@ -60,11 +54,9 @@ const Layout = () => {
         <Comment />
         <Collapse />
         <UCollLogo />
-        <Favorite />
       </BrowserRouter>
     </div>
   );
-
-  }
+};
 
 export default injectContext(Layout);
