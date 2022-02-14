c1cd66a6d5ee9fc909e73bddb079a8c3b03af98a
AslanSN <aslan.staub@manumartinez.es>
2022-02-14 20:24:51 +0100

    Tokens testing

--- src/js/component/Menus/ColleaguesSideMenu/ColleaguesSideMenu.jsx
+++ src/js/component/Menus/ColleaguesSideMenu/ColleaguesSideMenu.jsx
@@ -1,1 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import propTypes from 'propTypes';
+
+/**
+ * ! Side Menu -> Colleagues
+ * * AslanSN - 2022-02-14
+ * 
+ * @returns 
+ */
+const ColleaguesSideMenu = () => {
+    return (
+        <>
+        <div className="colleagues-side-menu">
+        
+        </div>
+        </>
+    )
+}
