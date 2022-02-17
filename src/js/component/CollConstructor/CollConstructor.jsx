772a27052632b23a9e54f62789751f9caaa68071
Ovidio Santoro <jogsantoro@gmail.com>
2022-02-17 21:08:54 +0100

    Created top third of the coll constructor

--- src/js/component/CollConstructor/CollConstructor.jsx
+++ src/js/component/CollConstructor/CollConstructor.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 
-import LikeButtons from "../Buttons/Like/LikeButtons.jsx";
-import Thread from "../Buttons/Thread/Thread.jsx";
-import Comment from "../Buttons/Comment/Comment.jsx";
-import Favorite from "../Buttons/Favorite/Favorite.jsx";
-import Share from "../Buttons/Share/Share.jsx";
+import NewPostDocument from "../Buttons/NewPostButtons/NewPostDocument.jsx";
+import NewPostLink from "../Buttons/NewPostButtons/NewPostLink.jsx";
+import NewPostSurvey from "../Buttons/NewPostButtons/NewPostSurvey.jsx";
+import NewPostText from "../Buttons/NewPostButtons/NewPostText.jsx";
+import NewPostVideo from "../Buttons/NewPostButtons/NewPostVideo.jsx";
+import NewPostPhoto from "../Buttons/NewPostButtons/NewPostPhoto.jsx";
 
 import "./CollConstructor.scss";
 
@@ -16,7 +17,23 @@ import "./CollConstructor.scss";
  */
 const CollConstructor = () => {
   // TODO: Delete Test class (Margin to view the component better)
-  return <div className="coll-constructor test"></div>;
+  return (
+    <form action="" className="coll-constructor test">
+      <div className="coll-constructor-title">
+        <input type="text" placeholder="Think of a good title!" />
+      </div>
+      {/* TODO: think of a good way to implement the icon next to the name. NOT PRIORITARY - FOR THE FINAL PRESENTATION */}
+      <div className="coll-constructor-selector">
+        <select id="classSelector">
+          <option value="biology2">Biology II</option>
+          <option value="microeconomics">Modern Microeconomics</option>
+          <option value="romanticlit">Romantic Literature</option>
+          <option value="sexed">Sex Education</option>
+        </select>
+        <div className="coll-constructor-buttons"></div>
+      </div>
+    </form>
+  );
 };
 
 export default CollConstructor;
