ff0cb2c29ae56421d9906c86e9825280079ae31b
Aslan Staub Negro <90191991+AslanSN@users.noreply.github.com>
2022-02-18 21:22:17 +0100

    Feature/interest-tag (#22)
    * branch and initial files created
    
    * Tag done, TODO: text adjustments
    
    * Text align centered

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,10 +5,13 @@ import injectContext from "./store/appContext";
 
 import HomePage from "./views/Home-Page/Home-Page.jsx";
 
+
+
 /**
  * ! Layout component for the views
  * * Template Component
  */
+
 const Layout = () => {
   //the basename is used when your project is published in a subdirectory and not in the root of the domain
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
@@ -16,6 +19,7 @@ const Layout = () => {
 
   return (
     <div>
+
       <BrowserRouter basename="/">
         <div>
           <Switch>
