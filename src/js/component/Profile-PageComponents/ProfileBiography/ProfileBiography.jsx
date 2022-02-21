38ae822743dcee36ee130329a2de8ad8a9d2ba82
Keili Rosales <keilirosalesr@gmail.com>
2022-02-21 10:24:41 +0100

    Creating files for the Profile Biography Component

---
+++ src/js/component/Profile-PageComponents/ProfileBiography/ProfileBiography.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import PropTypes from "prop-types";
+import "./ProfileBiography.scss";
+import ProfileEditButton from "../Buttons/ProfileEditButton/ProfileEditButton.jsx";
+
+/**
+ * ! Creates the Profile Biography
+ * * Keili Rosales - 2022-02-21
+ * @returns React Component
+ * @params Props {props}
+ * TODO: It is missing to add the props to connect it with the back. At the moment, it has only test data.
+ */
+
+const ProfileBiography = () => {
+  return (
+    <div className="profile-biography">
+      <div className="biography">
+      </div>    
+    </div>
+  );
+};
+
+export default ProfileBiography;
