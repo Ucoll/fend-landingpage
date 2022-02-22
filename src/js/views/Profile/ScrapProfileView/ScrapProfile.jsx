9ccef0a14f034fb1d24a730ce524abb94a4de9f9
Aslan Staub Negro <90191991+AslanSN@users.noreply.github.com>
2022-02-22 15:30:24 +0100

    Fixed duplicated files and ProBio Comp. (#31)

--- src/js/views/Profile/ScrapProfileView/ScrapProfile.jsx
+++ src/js/views/Profile/ScrapProfileView/ScrapProfile.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 import Cover from "../../../component/Profile-PageComponents/Cover/Cover.jsx";
+import ProfileBiography from "../../../component/Profile-PageComponents/ProfileBiography/ProfileBiography.jsx";
 import ProfileCareerDescription from "../../../component/Profile-PageComponents/ProfileCareerDescription/ProfileCareerDescription.jsx";
 import InterestTags from "../../../component/Profile-PageComponents/Tags/InterestTag.jsx";
-
+import ProfileClasses from "../../../component/Profile-PageComponents/ProfileClasses/ProfileClasses.jsx";
+import ProfileUserInfo from "../../../component/Profile-PageComponents/ProfileUserInfo/ProfileUserInfo.jsx";
+import ProfileFavorite from "../../../component/Profile-PageComponents/Buttons/ProfileFavorite/ProfileFavorite.jsx";
+import MyColls from "../../../component/StaticComponents/Buttons/MyColls/MyColls.jsx"
 
 /**
  * ! View -> Scrap Profile View
@@ -14,8 +18,13 @@ const ScrapProfile = () => {
   return (
     <>
       <Cover />
+      <MyColls />
+      <ProfileFavorite/>
       <ProfileCareerDescription />
       <InterestTags />
+      <ProfileBiography/>
+      <ProfileClasses />
+      <ProfileUserInfo />
     </>
   );
 };
