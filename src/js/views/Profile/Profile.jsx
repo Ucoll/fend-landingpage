263e55600d7af21b128453cdaa1c1fe9620508c3
Keili Rosales <keilirosalesr@gmail.com>
2022-03-07 23:17:34 +0100

    Profile view ready for review

--- src/js/views/Profile/Profile.jsx
+++ src/js/views/Profile/Profile.jsx
@@ -1,21 +1,70 @@
-import React from 'react';
+import React from "react";
 
-import Cover from '../../component/Profile-PageComponents/Cover/Cover.jsx';
-import ProfileCareerDescription from '../../component/Profile-PageComponents/ProfileCareerDescription/ProfileCareerDescription.jsx';
-import InterestTags from '../../component/Profile-PageComponents/Tags/InterestTag.jsx';
+import Navbar from "../../component/StaticComponents/Navbar/Navbar.jsx";
+import Cover from "../../component/Profile-PageComponents/Cover/Cover.jsx";
+import ProfileCareerDescription from "../../component/Profile-PageComponents/ProfileCareerDescription/ProfileCareerDescription.jsx";
+import ProfileBiography from "../../component/Profile-PageComponents/ProfileBiography/ProfileBiography.jsx";
+import SocialMedia from "../../component/Profile-PageComponents/SocialMedia/SocialMedia.jsx";
+import MyColls from "../../component/StaticComponents/Buttons/MyColls/MyColls.jsx";
+import ProfileFavorite from "../../component/Profile-PageComponents/Buttons/ProfileFavorite/ProfileFavorite.jsx";
+import ProfileClasses from "../../component/Profile-PageComponents/ProfileClasses/ProfileClasses.jsx";
+import ProfileUserInfo from "../../component/Profile-PageComponents/ProfileUserInfo/ProfileUserInfo.jsx"
+
+import "../Profile/ProfileStyles.scss";
+
 /**
  * ! View -> Profile
- * * AslanSN - 2022-02-18
- * 
+ * * AslanSN - 2022-02-18 / Keili Rosales 2022-02-23
+ *
  */
 const Profile = () => {
-    return (
-      <>
-        <Cover />
-        <ProfileCareerDescription />
-        <InterestTags />
-      </>
-    );
-}
+  return (
+    <>
+      <Navbar />
+      <div className="view-profile">
+        <div className="view-cover"><Cover /></div>
+        
+
+        <ul className="leftpostit">
+          <li className="left-careerdescription">
+            <ProfileCareerDescription />
+          </li>
+          <li className="left-biography">
+            <ProfileBiography />
+          </li>
+          <li className="left-socialmedia">
+            <SocialMedia />
+          </li>
+          <li className="left-socialmedia">
+            <SocialMedia />
+          </li>
+          <li className="left-socialmedia">
+            <SocialMedia />
+          </li>
+          <li className="left-socialmedia">
+            <SocialMedia />
+          </li>
+        </ul>
+        <ul className="rightpostit">
+          <li className="right-buttonmycolls">
+            <MyColls />
+          </li>
+          <li className="right-buttonfavorites">
+            <ProfileFavorite />
+          </li>
+          <li className="right-userinfo">
+            <ProfileUserInfo />
+          </li>
+          <li className="right-classes">
+            <ProfileClasses />
+          </li>
+          <li className="right-classestwo">
+            <ProfileClasses />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
--- src/js/views/Profile/ScrapProfileView/ScrapProfile.jsx
+++ src/js/views/Profile/ScrapProfileView/ScrapProfile.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 
-import Cover from "../../../component/Profile-PageComponents/Cover/Cover.jsx";
-import ProfileBiography from "../../../component/Profile-PageComponents/ProfileBiography/ProfileBiography.jsx";
-import ProfileCareerDescription from "../../../component/Profile-PageComponents/ProfileCareerDescription/ProfileCareerDescription.jsx";
-import SocialMedia from "../../../component/Profile-PageComponents/SocialMedia/SocialMedia.jsx";
-import ProfileSocialMediaText from "../../../component/Profile-PageComponents/SocialMedia/SocialMediaText.jsx";
-import InterestTags from "../../../component/Profile-PageComponents/Tags/InterestTag.jsx";
-import ProfileClasses from "../../../component/Profile-PageComponents/ProfileClasses/ProfileClasses.jsx";
-import ProfileUserInfo from "../../../component/Profile-PageComponents/ProfileUserInfo/ProfileUserInfo.jsx";
-import ProfileFavorite from "../../../component/Profile-PageComponents/Buttons/ProfileFavorite/ProfileFavorite.jsx";
-import MyColls from "../../../component/StaticComponents/Buttons/MyColls/MyColls.jsx"
+import Profile from "../Profile.jsx";
+import HomeView from "../../Home-Page/Home-Page.jsx";
 
 /**
  * ! View -> Scrap Profile View
@@ -19,16 +11,8 @@ import MyColls from "../../../component/StaticComponents/Buttons/MyColls/MyColls
 const ScrapProfile = () => {
   return (
     <>
-      <Cover />
-      <MyColls />
-      <ProfileFavorite/>
-      <ProfileCareerDescription />
-      <InterestTags />
-      <SocialMedia />
-      <ProfileSocialMediaText />
-      <ProfileBiography/>
-      <ProfileClasses />
-      <ProfileUserInfo />
+      <Profile />
+      {/* <HomeView /> */}
     </>
   );
 };
