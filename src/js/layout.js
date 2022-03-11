5903a5d2fd5bbbb334a3ff8e030a4f1a529db9cc
Keili Rosales <keilirosalesr@gmail.com>
2022-03-11 19:46:06 +0100

    Removed the scrap profile component and updated the layout

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import ProfileBiography from "./component/Profile-PageComponents/ProfileBiography/ProfileBiography.jsx";
 
 import injectContext from "./store/appContext";
 
@@ -9,8 +8,6 @@ import LandingPage from "./views/LandingPage/LandingPage.jsx";
 import Register from "./views/LandingPage/RegisterSubview/Register.jsx";
 import Messages from "./views/Messages/Messages.jsx";
 // import Profile from "./views/Profile/Profile.jsx";
-import ScrapProfile from "./views/Profile/ScrapProfileView/ScrapProfile.jsx";
-
 
 /**
  * ! Layout component for the views
@@ -29,7 +26,6 @@ const Layout = () => {
           <Switch>
             {/* <Route exact path="/" component={HomePage} /> */}
             {/* TODO: <Route exact path="/profile" component={Profile} /> */}
-            <ScrapProfile/>
             {/* <LandingPage/> */}
             {/* <Register/> */}
           </Switch>
